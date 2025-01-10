72343f8f27940a4c98394dc8eea61f4359700b87
AMAR KUMAR <155427107+AmarKumar2005@users.noreply.github.com>
2025-01-10 16:04:26 +0000

    project02

--- 07_project/project01/script.js
+++ 07_project/project01/script.js
@@ -6,26 +6,39 @@ buttons.forEach(function (button){
     button.addEventListener('click', function(event){
         console.log(event);
         console.log(event.target);
-        if(event.target.id == 'grey'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'white'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'blue'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'yellow'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'darkgreen'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'fuchsia'){
-            body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id == 'chocolate'){
-            body.style.backgroundColor = event.target.id;
+        // if(event.target.id == 'grey'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'white'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'blue'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'yellow'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'darkgreen'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'fuchsia'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if(event.target.id == 'chocolate'){
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        switch(event.target.id) {
+            case 'grey':
+            case 'white':
+            case 'blue':
+            case 'yellow':
+            case 'darkgreen':
+            case 'fuchsia':
+            case 'chocolate':
+                body.style.backgroundColor = event.target.id;
+                break;
+            default:
+                console.log('Unknown color');
         }
     });
 });
