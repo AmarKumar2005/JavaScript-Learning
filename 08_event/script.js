e42cc052639072672735c7fc4da3959254aad5cf
AMAR KUMAR <155427107+AmarKumar2005@users.noreply.github.com>
2025-03-13 19:20:29 +0000

    update_event

--- 08_event/script.js
+++ 08_event/script.js
@@ -1,14 +1,28 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () =>{
-    console.log("btn1 was clicked");
-    let a =25;
-    a++;
-    console.log(a);
-}
+// btn1.onclick = () =>{
+//     console.log("btn1 was clicked");
+//     let a =25;
+//     a++;
+//     console.log(a);
+// }
 
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
 
-div.onmouseover = () =>{
-    console.log('you are inside div')
-}
+// div.onmouseover = () =>{
+//     console.log('you are inside div')
+// }
+
+let modebtn = document.querySelector("#mode")
+let currMode = "light";
+modebtn.addEventListener("click", () =>{
+    if(currMode === 'light'){
+        currMode = 'Dark';
+        document.querySelector('body').style.backgroundColor = 'black';
+    } else{
+        currMode = 'light';
+        document.querySelector('body').style.backgroundColor = 'white';
+    }
+    console.log(currMode);
+    
+})
